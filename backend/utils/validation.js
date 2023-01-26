dcad5e7d8f1dd9afa82ae5939768d887337f9b45
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-01-25 19:24:19 -0500

    get current user double checked after changes

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,6 +1,6 @@
 // backend/utils/validation.js
-const { validationResult } = require('express-validator');
-
+const {body, validationResult } = require('express-validator');
+console.log(validationResult)
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, res, next) => {
@@ -11,20 +11,25 @@ const handleValidationErrors = (req, res, next) => {
       .array()
       .map((error) => `${error.msg}`);
 
-    const err = Error('Bad request.');
+    const err = Error('');
     err.errors = errors;
-    err.status = 400;
-    err.title = 'Bad request.';
-    // next(err); //generic response to satisfy endpoint for login errors
-    res.statusCode = 400
-    res.json({
-      "message":"Validation error",
-      "statusCode":400,
-      "errors":{
-        "credential":"Email or username is requried",
-        "password":"Password is required"
-      }
-    })
+
+    //unique email/username validation response //log in\\
+    if((err.errors[0].toString().includes("User with that email already exists"))||(err.errors[0].toString().includes("User with that username already exists"))){
+      err.message = "User already exists"
+      err.statusCode = 403
+      res.statusCode = 403
+      return res.json({"message":err.message,"statusCode":err.statusCode,"errors":err.errors})
+    }
+
+
+
+
+    //basic validation error res
+     err.message = "Validation Error"
+     err.status = 400;
+     res.statusCode = 400
+    return res.json({"message":err.message,"statusCode":err.status,"errors":err.errors})
   }
   next();
 };
