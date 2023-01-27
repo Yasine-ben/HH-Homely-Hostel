bc4c7db55376f3793e2229decd5d509f79770778
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-01-27 16:31:13 -0500

    add image to a review complete

--- backend/db/seeders/20230124225101-demo-review-images.js
+++ backend/db/seeders/20230124225101-demo-review-images.js
@@ -9,14 +9,14 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'ReviewImages';
     return queryInterface.bulkInsert(options, [
-      { reviewId: 2, url: 'https://loremflickr.com/640/480/cats' },
-      { reviewId: 5, url: 'https://loremflickr.com/640/480/cats' },
-      { reviewId: 10, url: 'https://loremflickr.com/640/480/cats' },
       { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
-      { reviewId: 4, url: 'https://loremflickr.com/640/480/cats' },
-      { reviewId: 9, url: 'https://loremflickr.com/640/480/cats' },
-      { reviewId: 7, url: 'https://loremflickr.com/640/480/cats' },
-      { reviewId: 9, url: 'https://loremflickr.com/640/480/cats' },
+      { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
+      { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
+      { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
+      { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
+      { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
+      { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
+      { reviewId: 1, url: 'https://loremflickr.com/640/480/cats' },
       { reviewId: 10, url: 'https://loremflickr.com/640/480/cats' },
       { reviewId: 7, url: 'https://loremflickr.com/640/480/cats' }
     ], {});
