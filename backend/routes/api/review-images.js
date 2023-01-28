0406bcb1ad476ed54b994358bd91f9a741d198e3
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-01-27 21:35:21 -0500

    delete review image complete

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,4 +1,4 @@
-// backend/routes/api/spot-images.js
+// backend/routes/api/review-images.js
 const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
@@ -8,29 +8,30 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// Delete spot image
-// Requires authentication
-// Spot must belong to current user
+// Delete a review image
+// Requires Authentication
+// User must own the review
 // Complete
-router.delete('/:imageId',requireAuth, async(req,res) => {
-    const image = await SpotImage.findAll({where:{id:req.params.imageId},include:[{model:Spot}]})
-    //res.json(image)
+router.delete('/:imageId', requireAuth, async(req,res) => {
+    const image = await ReviewImage.findAll({where:{id:req.params.imageId},include:[{model:Review}]})
+
     if(image[0]){
-        if(image[0].Spot.ownerId == req.user.id){
-            delete image[0].spot
+        if(image[0].Review.userId == req.user.id){
+            delete image[0].Review
             await image[0].destroy()
             res.statusCode = 200
             res.json({"message":"Successfully deleted","statusCode":res.statusCode})
         }else{
             res.statusCode = 404
-            res.json({"message":"You do not own this spot","statusCode":res.statusCode})
+            res.json({"message":"You do not own this review","statusCode":res.statusCode})
         }
     }else{
         res.statusCode = 404
-        res.json({"message":"Spot Image couldn't be found","statusCode":res.statusCode})
+        res.json({"message":"Review Image couldn't be found","statusCode":res.statusCode})
     }
     
 })
+
 
 
 module.exports = router;
