45d471a099b333af23e5b4e71c343a6222020a3e
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-01-27 21:18:11 -0500

    delete spot image complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js')
-const reviewsRouter = require('./reviews')
+const reviewsRouter = require('./reviews.js')
+const spotImageRouter = require('./spot-images.js')
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -16,6 +17,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter)
 router.use('/reviews', reviewsRouter)
+router.use('/spot-images', spotImageRouter)
+
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
