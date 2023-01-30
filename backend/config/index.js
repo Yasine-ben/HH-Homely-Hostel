72632f44b570fcbc4b40fec98ac00212624042fa
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-01-29 19:51:26 -0500

    added options to all migrations

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,6 +1,6 @@
 // backend/config/index.js
 module.exports = {
-    environment: process.env.NODE_ENV || 'production',
+    environment: process.env.NODE_ENV || 'developement',
     port: process.env.PORT || 8000,
     dbFile: process.env.DB_FILE,
     jwtConfig: {
