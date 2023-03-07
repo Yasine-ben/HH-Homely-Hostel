4b9b027c693eb1233f9897caba25d27f78bd4905
Yasine Omar Benzekri <yasineomarbenzekri@outlook.com>
2023-03-07 11:21:04 -0500

    continuing csss on main tool bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,24 +1,30 @@
 // frontend/src/components/Navigation/index.js
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from "../assets/logo.png"
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-      </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
-    </ul>
+    <>
+      <div className="header">
+        <NavLink ClassName="logo" exact to="/"> <img src={logo} height="50" width="50"/> </NavLink>
+        <nav className="navigation">
+          <NavLink class="nav-link" to="/">Link-1</NavLink>
+          <NavLink class="nav-link" to="/">Link-2</NavLink>
+          <NavLink class="nav-link" to="/">Link-3</NavLink>
+          
+        </nav>
+        <buttons>
+          {isLoaded && (<ProfileButton classname="profile-btn" user={sessionUser} />)}
+        </buttons>
+      </div>
+    
+    </>
   );
 }
 
